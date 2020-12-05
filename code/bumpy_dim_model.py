23b83201bd7e6f7d05b17e04193d5d9b9ad57385
Thomas Jenkins <tjenkin3@cs.brown.edu>
2020-12-05 04:16:56 -0500

    Committer: Thomas Jenkins <tjenkin3@cs.brown.edu>  On branch main  Your branch is up to date with 'origin/main'.  Changes to be committed: 	modified:   bumpy_dim_model.py

--- code/bumpy_dim_model.py
+++ code/bumpy_dim_model.py
@@ -16,17 +16,21 @@ class Generator(tf.keras.Model):
         self.num_im_feats=2048
         self.hidden_size=1024
         self.out_size=85
-        self.dropout_rate=.01 #figure out what dropout rate to use. fine tuning?
+        self.dropout_rate=.5 #figure out what dropout rate to use. fine tuning?
+        self.num_iterations=3
+
+
         #TODO figure out args of resnet initialization, got to output right shape.
         self.ResNet=tf.keras.applications.ResNet50V2(classes=self.num_im_feats)
-
-
+        self.optimizer=tf.keras.optimizers.Adam(learning_rate=self.learning_rate)
+        
 
 
         #3D module (as referred to in paper)
         self.layer1=tf.keras.layers.Dense(self.hidden_size,activation='relu')
         self.drop1=tf.keras.layers.Dropout(self.dropout_rate,input_shape=(self.hidden_size,))
         self.layer2=tf.keras.layers.Dense(self.hidden_size,activation='relu')
+        self.drop2=tf.keras.layers.Dropout(self.dropout_rate,input_shape=(self.hidden_size,))
         self.layer3=tf.keras.layers.Dense(self.out_size)
     def IEF(self,imFeats,estimate):
         #perform one iteration of 3d regression
@@ -39,6 +43,7 @@ class Generator(tf.keras.Model):
         reg=self.layer1(vect)
         reg=self.drop1(reg)
         reg=self.layer2(reg)
+        reg=self.drop2(reg)
         reg=self.layer3(reg)
         curr_est+=reg
         return curr_est
@@ -48,13 +53,20 @@ class Generator(tf.keras.Model):
         #might also want to reshape images here? this function might be unnecessary.
         features=self.ResNet.call(img)
         return features
+    def init_param_est(self):
+        est=[]
+        #returns the initial parameter estimates. 
+        return est
     def call(self,x):
         #use functions previously defined to extract features, the run said features.
         #output 85 dim vector, returned by iterative regression. 
         #make sure to output in correct shape for SMPL or STAR, then to
         features=self.extract_features(x)
+        curr_est=self.init_param()
+        for i in range(self.num_iterations):
+            curr_est=self.IEF(features,curr_est)
         
-        pass
+        return curr_est
     def loss(self,x,y,z):
         #this loss will not be the total loss, which depends on the discriminator
         #This loss will only be the reprojections loss (maybe)
@@ -83,6 +95,4 @@ class Discriminator(tf.keras.Model):
         # for now, inputs are the generated SMPL/STAR params, and the real mesh params from unpaired dataset.
 
         pass 
-
-
 
