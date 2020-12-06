638fd186163e3f80d64e475ed7d39d0514ce46e5
swannyswan <71267505+swannyswan@users.noreply.github.com>
2020-12-06 13:00:12 -0500

    Update lsp_preprocess.py

--- code/lsp_preprocess.py
+++ code/lsp_preprocess.py
@@ -5,4 +5,17 @@ import random
 import math
 import scipy.io as sio
 
-def get_lsp(filepath):
+from os.path import join #, exists (p cut)
+
+def get_lsp(data_path):
+  
+  # load annotation matrices (hopefully)
+  
+  ''' Note: file joints.mat is MATLAB data file with joint annotations in 3x14x10000 matrix ('joints')
+  with x and y locations and binary value indicating visbility of each joint '''
+  
+  annotations = join(data_path, 'joints.mat')
+  joints = sio.loadmat(annotations)['joints']
+  
+  
+                         
