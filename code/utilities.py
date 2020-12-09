fc6a8d09b82263e251e610570cbeef9709b5cf80
traliegh5 <57418419+traliegh5@users.noreply.github.com>
2020-12-09 01:07:20 -0500

    On branch main  Your branch is up to date with 'origin/main'.  Changes to be committed: 	modified:   utilities.py

--- code/utilities.py
+++ code/utilities.py
@@ -3,7 +3,7 @@ import tensorflow as tf
 import numpy as np
 import random
 import math
-
+import tensorflow_graphics as tfg 
 
 """this model can be for all intermediate processing methods, potentially. things like projection, rodriguez formula,
 maybe smpl/STAR stuff? all of the math transformations we need to do can be located here. If this structure is unnecessary 
@@ -12,7 +12,7 @@ then feel free to do it another waydone
 
 
 
-def map():
+def map(meshes):
     """it doesn't need to be done here, but we need to create texture maps. To do this,we need to project the mesh back into the images
     space, then convolve over it with a "bilinear sampling kernel" which estimates texture. we can look at differences 
     across texture maps of consecutive images as loss. """"
@@ -23,3 +23,12 @@ def map():
     #step3: output texture map
 
     pass
+def orth_project(params):
+    app=None
+    scale=params[-1]
+    pose=params[:24]
+
+
+    return app
+
+
