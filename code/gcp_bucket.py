4f4e30230199d950eef6c49e191caee06eb28e23
gregory_barboy@brown.edu <gregory_barboy@brown.edu>
2021-01-16 19:58:27 -0500

    Added gcp load

---
+++ code/gcp_bucket.py
@@ -0,0 +1,30 @@
+from google.cloud import storage
+
+
+def download_blob(bucket_name, source_blob_name, destination_file_name):
+    """Downloads a blob from the bucket."""
+    # bucket_name = "your-bucket-name"
+    # source_blob_name = "storage-object-name"
+    # destination_file_name = "local/path/to/file"
+
+    storage_client = storage.Client()
+
+    bucket = storage_client.bucket(bucket_name)
+
+    # Construct a client side representation of a blob.
+    # Note `Bucket.blob` differs from `Bucket.get_blob` as it doesn't retrieve
+    # any content from Google Cloud Storage. As we don't need additional data,
+    # using `Bucket.blob` is preferred here.
+    blob = bucket.blob(source_blob_name)
+    blob.download_to_filename(destination_file_name)
+
+    print(
+        "Blob {} downloaded to {}.".format(
+            source_blob_name, destination_file_name
+        )
+    )
+ 
+def main():
+    download_blob('neutr_mosh_unpickled', 'shapes_netruSMPL_CMU.txt', 'test.txt')
+if __name__ == '__main__':
+    main()
