d0ed9fc64abe27c060d9892b79f65f463ea3949a
gregory_barboy@brown.edu <gregory_barboy@brown.edu>
2021-01-19 17:20:55 -0500

    Meshes print

--- code/bumpy_dim_runner.py
+++ code/bumpy_dim_runner.py
@@ -169,6 +169,8 @@ def saveMesh(params):
     outmesh_path = './test_smpl.obj'
     with open( outmesh_path, 'w') as fp:
         for v in m[0]:
+            print(m[0])
+            print(v)
             fp.write( 'v %f %f %f\n' % ( v[0], v[1], v[2]) )
 
         for f in star.f+1: # Faces are 1-based, not 0-based in obj files
