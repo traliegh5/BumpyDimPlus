0cbec0f2c8b6662b16c6c2b2f6a75ebf0561841e
traliegh5 <57418419+traliegh5@users.noreply.github.com>
2021-01-18 21:58:59 -0500

    fixing

--- code/bumpy_dim_model.py
+++ code/bumpy_dim_model.py
@@ -122,13 +122,15 @@ class Discriminator(tf.keras.Model):
 
         poseEmb=self.pE1(poses)
         poseEmb=self.pE2(poseEmb)
-        print(tf.shape(poseEmb))
+        #print(tf.shape(poseEmb))
         poseDisc=[]
         for i in range(self.num_joints):
-            poseDisc.append(self.jointDiscList[i](poseEmb[:,i,:,:]))
-        print(tf.shape(poseDisc))
+            temp=self.jointDiscList[i](poseEmb[:,i,:,:])
+            print(tf.shape(temp))
+            poseDisc.append(temp)
+        #print(tf.shape(poseDisc))
         poseDisc=tf.squeeze(tf.stack(poseDisc,axis=1))
-        print(tf.shape(poseDisc))
+        #print(tf.shape(poseDisc))
         poseEmb=self.flatten(poseEmb)
         allPoseDisc=self.poseD1(poseEmb)
         allPoseDisc=self.poseD2(allPoseDisc)
