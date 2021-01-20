aaf0263daaf59902c8944cd60b89c5a29266a55e
gregory_barboy@brown.edu <gregory_barboy@brown.edu>
2021-01-19 21:27:05 -0500

    float

--- code/render_loss.py
+++ code/render_loss.py
@@ -11,9 +11,9 @@ f.close()
 
 x_array = np.arange(len(contents_dis))
 
-contents_gen = np.array(contents_gen)
+contents_gen = np.array(contents_gen, dtype=np.float32)
 plt.plot(x_array, contents_gen, color='blue')
 
-contents_dis = np.array(contents_dis)
+contents_dis = np.array(contents_dis, dtype=np.float32)
 plt.plot(x_array, contents_dis, color='green')
 plt.savefig('loss_plot.png')
